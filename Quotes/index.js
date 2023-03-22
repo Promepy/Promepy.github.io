2d94545a976e0b80c8d831573f79e903123f529c
Pradeepthi74 <pradeepthi.kondri77@gmail.com>
2023-03-23 02:24:30 +0530

    Updated Quotes

--- Quotes/index.js
+++ Quotes/index.js
@@ -1,16 +1,43 @@
 window.onload = function () {
-    readQuotes();
+    var data = JSON.parse(localStorage.getItem("data"));
+    if (!data) {
+        console.log("No issue if");
+        readQuotes();
+    }
+    displayQuote();
 };
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 readQuotes = () => {
-    fetch("http://127.0.0.1:5500/quotes.json")
+    console.log("No Issue");
+    fetch("https://raw.githubusercontent.com/Promepy/Promepy.github.io/master/Quotes/quotes.json")
         .then(response => response.json())
         .then(data => {
-            console.log(data.length());
+            var i = 1;
+            var quotes = [];
             data.forEach(item => {
-                console.log(item.Quote, item.Author);
+
+                var quote = { Quote: item.Quote, Author: item.Author };
+                if (i < 20001) {
+                    //localStorage.setItem("data"+i, );
+                    quotes.push(quote);
+                }
+                i++;
             });
+            localStorage.setItem("data", JSON.stringify(quotes));
+
         })
         .catch(error => console.error(error));
+}
+
+displayQuote = () =>{
+    var target = document.querySelector('div');
+    let x = Math.round(Math.random() * (20000));
+    data = JSON.parse(localStorage.getItem("data"));
+    console.log(x);
+    console.log(data[x].Quote, data[x].Author);
+    target.innerHTML = data[x].Quote + " - " + data[x].Author;
 }
